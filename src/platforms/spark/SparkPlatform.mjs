97e425b2fa3e0d9cb482262fa69d14215beeee38
Tomasz Kumor <tomasz.kumor@redembedded.com>
2019-11-14 13:56:12 +0100

    add export default

--- src/platforms/spark/SparkPlatform.mjs
+++ src/platforms/spark/SparkPlatform.mjs
@@ -2,7 +2,7 @@ import fs from "fs";
 import http from "http";
 import https from "https";
 
-export class SparkMediaplayer extends lightning.Component {
+export default class SparkMediaplayer extends lightning.Component {
 
     _construct(){
         this._skipRenderToTexture = false;
