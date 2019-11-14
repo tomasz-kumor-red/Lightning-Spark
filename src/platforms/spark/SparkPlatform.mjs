b3f58daf15c0a2407f6e8566f675c7a2b1fdcc1f
Tomasz Kumor <tomasz.kumor@redembedded.com>
2019-11-14 14:28:02 +0100

    Revert "add export default"
    This reverts commit 97e425b2fa3e0d9cb482262fa69d14215beeee38.

--- src/platforms/spark/SparkPlatform.mjs
+++ src/platforms/spark/SparkPlatform.mjs
@@ -2,7 +2,7 @@ import fs from "fs";
 import http from "http";
 import https from "https";
 
-export default class SparkMediaplayer extends lightning.Component {
+export class SparkMediaplayer extends lightning.Component {
 
     _construct(){
         this._skipRenderToTexture = false;
