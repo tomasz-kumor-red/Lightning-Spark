201f3af91e0954f089604cc0393f79ee9273f315
Tomasz Kumor <tomasz.kumor@redembedded.com>
2019-11-14 10:11:14 +0100

    fix path

--- src/platforms/spark/SparkPlatform.mjs
+++ src/platforms/spark/SparkPlatform.mjs
@@ -1,7 +1,7 @@
 import fs from "fs";
 import http from "http";
 import https from "https";
-import SparkMediaplayer from 'Sparkmediaplayer'
+import SparkMediaplayer from "./Sparkmediaplayer";
 
 export default class SparkPlatform {
 
