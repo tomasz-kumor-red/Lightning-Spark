ff4442e7016c2f55906569ce8d3fa891e1eeb6b9
Tomasz Kumor <tomasz.kumor@redembedded.com>
2019-11-15 09:44:45 +0100

    change import position

--- src/lightning.mjs
+++ src/lightning.mjs
@@ -3,6 +3,8 @@ import SparkPlatform from "./platforms/spark/SparkPlatform.mjs"
 
 const lightning = lng;
 
+import SparkMediaplayer from "./platforms/spark/SparkMediaplayer.mjs"
+
 lightning.Stage.platform = SparkPlatform;
 
 export default lightning;
