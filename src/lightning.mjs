5a4772206d96c24c76616924137640304e659fb1
Tomasz Kumor <tomasz.kumor@redembedded.com>
2019-11-15 12:03:07 +0100

    remove SparkMediaplayer import

--- src/lightning.mjs
+++ src/lightning.mjs
@@ -1,6 +1,5 @@
 import lng from "wpe-lightning/src/lightning.mjs";
 import SparkPlatform from "./platforms/spark/SparkPlatform.mjs"
-import SparkMediaplayer from "./platforms/spark/SparkMediaplayer.mjs"
 
 const lightning = lng;
 
